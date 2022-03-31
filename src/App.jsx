0ac0497c45dfb8e63a9d07717dea82ae0a7970b1
barbosadiego <diego.gutemberg@hotmail.com>
2022-03-31 09:13:23 -0300

    estrutura inicial

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Home from './components/Home'
+import Login from './components/Login/Login'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 import './App.css'
-import Api from './api/Api.jsx'
 
 const App = (props) => {
   return (
     <div>
-      <Api />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   )
 }
