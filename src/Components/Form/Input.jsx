fa363166846162d9156cb7d735f152d8867e4557
barbosadiego <diego.gutemberg@hotmail.com>
2022-04-06 08:59:46 -0300

    use form

--- src/Components/Form/Input.jsx
+++ src/Components/Form/Input.jsx
@@ -1,14 +1,24 @@
-import React from 'react'
-import './Input.css'
+import React from 'react';
+import './Input.css';
 
-const Input = ({label, type, name}) => {
+const Input = ({ label, type, name, value, onChange, error, onBlur }) => {
   return (
-    <div className='input-wrapper'>
-      <label htmlFor={name} className='input-label'>{label}</label>
-      <input id={name} name={name} className='input-input' type={type}/>
-      <p className='input-error'>Error</p>
+    <div className="input-wrapper">
+      <label htmlFor={name} className="input-label">
+        {label}
+      </label>
+      <input
+        id={name}
+        name={name}
+        className="input-input"
+        type={type}
+        value={value}
+        onChange={onChange}
+        onBlur={onBlur}
+      />
+      {error && <p className="input-error">{error}</p>}
     </div>
-  )
-}
+  );
+};
 
-export default Input
+export default Input;
